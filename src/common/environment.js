5efa95bc88266396ae2da1bef2fb153f325ecc9f
CodingGriffin <biggriffin640@gmail.com>
2024-09-20 19:51:41 +0200

    modify api

--- src/common/environment.js
+++ src/common/environment.js
@@ -17,7 +17,7 @@ if (env === "local") {
     channel: "web",
     name: "Development",
     uiHost: "http://localhost:5173",
-    apiHost: "https://energy-api-psi.vercel.app",
+    apiHost: "https://energy-api-ko7d.onrender.com",
     assetBase: "/assets",
   };
 }
